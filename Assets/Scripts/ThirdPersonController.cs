3ba09917bda75a22124dc2570e458ad7b5b11670
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2024-10-06 13:58:12 -0600

    Añadiendo modelos y corrigiendo archivos

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -51,7 +51,7 @@ public class ThirdPersonController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed;
+            float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed; 
             controller.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
             animator.SetBool("IsRun",true);
         }else
